8b9ebd00add33ad4cc0373ff9244bb50ce7963c3
Javier Noris <norisjavier@gmail.com>
2015-04-04 01:49:31 -0700

    configuring assets precompiler initializer for heroku

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module Usalightingsystems
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Assets on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
