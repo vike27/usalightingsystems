a1945c140d485fc056b2bdd94f5a7620ca278499
Javier Noris <norisjavier@gmail.com>
2015-04-04 01:59:13 -0700

    configuring assets precompiler initializer for heroku again

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Usalightingsystems
   class Application < Rails::Application
     
+    config.assets.initialize_on_precompile = false
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -35,8 +36,5 @@ module Usalightingsystems
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
-    # Assets on Heroku
-    config.assets.initialize_on_precompile = false
   end
 end
